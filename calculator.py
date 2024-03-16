210d3e919d2074d1ee59371a823df61e9c6e1b8d
Madhavaggarwal0007 <152834808+Madhavaggarwal0007@users.noreply.github.com>
2024-03-16 12:15:23 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -13,10 +13,7 @@ def multiply(n1, n2):
 def divide(n1, n2):
     if n2 != 0:
         return n1 / n2
-    else:
-        raise ValueError("Cannot divide by zero")
-    
-    #this function calculate the product 
+#this function calculate the product 
 def calculator():
     operations = {
         '+': add,
@@ -28,7 +25,7 @@ def calculator():
     num1 = float(input("Enter first number: "))
     op = input("Enter operator (+,-,*,/): ")
     num2 = float(input("Enter second number: "))
-#Printing tge result
+
     if op in operations:
         result = operations[op](num1, num2)
         print(f"{num1} {op} {num2} = {result}")
